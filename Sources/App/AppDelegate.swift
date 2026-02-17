961166e6b047841e5911545edc0fe6b1f0285527
rinsuki <428rinsuki+git@gmail.com>
2026-02-17 12:40:03 +0900

    feat: add (very bad) onboarding
    its very bad, but better than just crashing.

--- Sources/App/AppDelegate.swift
+++ Sources/App/AppDelegate.swift
@@ -26,9 +26,31 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func activateOpenXRRuntime() {
-        let openXRRuntimeJSONPath = "/usr/local/share/openxr/1/active_runtime.json"
+        let openXRDir = "/usr/local/share/openxr/1"
+        let openXRRuntimeJSONPath = "\(openXRDir)/active_runtime.json"
         let ourRuntimeJSONPath = Bundle.main.url(forResource: "openxr_manifest", withExtension: "json")!.path()
-        try? FileManager.default.createDirectory(atPath: "/usr/local/share/openxr/1", withIntermediateDirectories: true, attributes: nil)
+        while !FileManager.default.fileExists(atPath: openXRDir) {
+            do {
+                try FileManager.default.createDirectory(atPath: openXRDir, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                // TODO: replace with better onboarding
+                let alert = NSAlert()
+                alert.alertStyle = .critical
+                alert.messageText = "Please create \(openXRDir)"
+                alert.informativeText = "\(openXRDir) is not exists, and this app doesn't have a permission to create it.\n\nYou need to create the folder by run this script in the Terminal.app:\n\nsudo mkdir -p \(openXRDir) && sudo chown \"$(whoami)\" \(openXRDir)\n\nAfter that, Please click \"Retry\"."
+                let retryButton = alert.addButton(withTitle: "Retry")
+                retryButton.keyEquivalent = "\r"
+                retryButton.tag = NSApplication.ModalResponse.OK.rawValue
+                let cancelButton = alert.addButton(withTitle: "Quit")
+                cancelButton.keyEquivalent = "q"
+                cancelButton.keyEquivalentModifierMask = .command
+                cancelButton.tag = NSApplication.ModalResponse.cancel.rawValue
+                if alert.runModal() != .OK {
+                    NSApplication.shared.terminate(self)
+                    return
+                }
+            }
+        }
         // read runtime json's destination of symlink
         let destination = try? FileManager.default.destinationOfSymbolicLink(atPath: openXRRuntimeJSONPath)
         if let destination {
