929bee1323c63991b447cba625b1ae4175ef9880
rinsuki <428rinsuki+git@gmail.com>
2026-02-20 17:28:12 +0900

    chore: add friendly label to MTLCommandQueue

--- Sources/Runtime/XRSession.swift
+++ Sources/Runtime/XRSession.swift
@@ -39,6 +39,7 @@ class XRSession {
         case XR_TYPE_GRAPHICS_BINDING_METAL_KHR:
             let next = createInfo.next.bindMemory(to: XrGraphicsBindingMetalKHR.self, capacity: 1).pointee
             let commandQueue = Unmanaged<MTLCommandQueue>.fromOpaque(.init(next.commandQueue)).takeUnretainedValue()
+            commandQueue.label = (commandQueue.label ?? "") + " (Attached to FruitXR XrSession \(self))"
             graphicsAPI = GraphicsAPI.metal(commandQueue: commandQueue)
             break
         default:
